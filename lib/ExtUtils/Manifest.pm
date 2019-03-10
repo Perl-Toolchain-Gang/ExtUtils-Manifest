0167495fbcc9c4937cd113f6d3cfc06befc303d2
Karen Etheridge <ether@cpan.org>
2019-03-10 13:12:11 -0700

    use more efficient expr form of map, grep when possible

--- lib/ExtUtils/Manifest.pm
+++ lib/ExtUtils/Manifest.pm
@@ -690,7 +690,7 @@ sub maniadd {
     _fix_manifest($MANIFEST);
 
     my $manifest = maniread();
-    my @needed = grep { !exists $manifest->{$_} } keys %$additions;
+    my @needed = grep !exists $manifest->{$_}, keys %$additions;
     return 1 unless @needed;
 
     open(MANIFEST, ">>$MANIFEST") or
